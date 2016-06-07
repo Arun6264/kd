d97e3cae2395e8df917a1e9d70e6a5ea90b9cbd8
E.Arun Anantha Raj <earunanantharaj@gmail.com>
2016-06-07 11:42:41 +0530

    Update kd.c

--- kd.c
+++ kd.c
@@ -1,30 +1,8 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-long long int cumfun(const void * a, const void * b)
+void main()
 {
-   return ( *(long long int*)a - *(long long int*)b );
-}
-
-main()
-{
-long long int arr[10001],number,i,k,l,fg=0;
-char str[10001];
-scanf("%s",str);
-scanf("%lld",&k);
-l=strlen(str);
-for(i=0;i<l;i++)
-    arr[i]=(int)str[i]%48;
-qsort(arr,l,sizeof(long long int), cumfun);
-for(i=0;i<l-k;i++)
-{
-if(arr[i]!=0){
-printf("%lld",arr[i]);
-fg=1;}
-if(arr[i]==0&&fg==0)
-    fg=0;
-}
-if(fg==0)
-printf("0");
-return 0;
+   int n,i,sum;
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+     sum=sum*i;
 }
